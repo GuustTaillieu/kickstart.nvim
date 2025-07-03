a61f73853d6cf97b91007dd7bbcee028630b3ec6
Gustavo <thegutagamer@taillieusauw.net>
2025-07-03 14:05:24 +0200

    added a few more plugins to make my life easier :)

---
+++ lua/custom/plugins/autotags.lua
@@ -0,0 +1,25 @@
+return {
+  'windwp/nvim-ts-autotag',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('nvim-ts-autotag').setup {
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+        filetypes = {
+          'html',
+          'xml',
+          'javascript',
+          'javascriptreact',
+          'typescript',
+          'typescriptreact',
+          'vue',
+          'svelte',
+        },
+      },
+    }
+  end,
+  event = 'InsertEnter',
+}
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,14 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        dockerfile = { 'hadolint' },
+        json = { 'jsonlint' },
+        rst = { 'vale' },
+        text = { 'vale' },
+        javascript = { 'eslint' },
+        javascripttreact = { 'eslint' },
+        typescript = { 'eslint' },
+        typescriptreact = { 'eslint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -30,16 +38,15 @@ return {
       -- }
       --
       -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
+        lint.linters_by_ft['markdown'] = nil
+        lint.linters_by_ft['dockerfile'] = nil
+        lint.linters_by_ft['json'] = nil
+        lint.linters_by_ft['rst'] = nil
+        lint.linters_by_ft['text'] = nil
+        lint.linters_by_ft['javascript'] = nil
+        lint.linters_by_ft['javascripttreact'] = nil
+        lint.linters_by_ft['typescript'] = nil
+        lint.linters_by_ft['typescriptreact'] = nil
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
